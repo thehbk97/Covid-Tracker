2388f1784effaccde0df40f34fc70a39590d8726
ZeeSneaks <zeesneaks@gmail.com>
2020-08-12 18:24:59 +0100

    Code files

---
+++ CourseworkModules.js
@@ -0,0 +1,27 @@
+var util = require('util');
+var fs = require('fs');
+
+const getInfo=()=>
+{
+    
+}
+
+const readFile=()=>
+{
+
+}
+
+const showImage=()=>
+{
+
+}
+
+const showVideo=()=>
+{
+    
+}
+
+exports.getInfo=getInfo
+exports.readFile=readFile
+exports.showImage=showImage
+exports.showVideo=showVideo
